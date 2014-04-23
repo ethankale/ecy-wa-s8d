291701e4d3a5cd6dc0783f1713b481af53310b68
Nat Kale <nathanielkale@gmail.com>
2014-04-23 12:46:26 -0700

    Updated .gitingnore, improved volume cals
    The .gitignore file now excludes .Rproj files and .Rproj.user directories.
    SQL for load has also been refined.

--- scripts/Load_byParam_sub1_lookup.R
+++ scripts/Load_byParam_sub1_lookup.R
@@ -8,9 +8,13 @@ require(sqldf)
 
 ### convert from text to date format:
 Storm$Field_Collection_Start_Date <- as.Date(Storm$Field_Collection_Start_Date, "%m/%d/%Y")
-Storm$Field_Collection_End_Date <- as.Date(Storm$Field_Collection_End_Date, "%m/%d/%Y")
+Storm$Field_Collection_End_Date   <- as.Date(Storm$Field_Collection_End_Date, "%m/%d/%Y")
 
-table(Storm$Parameter)
+### Dates as numbers (days since 1970/01/01), to make things simple for the SQL:
+Storm$start <- as.numeric(Storm$Field_Collection_Start_Date)
+Storm$end   <- as.numeric(Storm$Field_Collection_End_Date)
+
+#table(Storm$Parameter)
 
 ####test whether the sample volume is greater than the storm
 #sample_storm<-Storm[,c("Parameter","Location_ID","Field_Collection_Start_Date","new_Result_Value")]
@@ -29,6 +33,8 @@ storm_event_flows  <- Storm[which(Storm$Parameter == "Storm Event Flow Volume"),
                             c("Location_ID", 
                               "Field_Collection_Start_Date", 
                               "Field_Collection_End_Date", 
+                              "start",
+                              "end",
                               "new_Result_Value")
                             ]
 
@@ -36,6 +42,8 @@ sample_event_flows <- Storm[which(Storm$Parameter == "Sampled-Event Flow Volume"
                             c("Location_ID", 
                               "Field_Collection_Start_Date", 
                               "Field_Collection_End_Date", 
+                              "start",
+                              "end",
                               "new_Result_Value")
                             ]
 
@@ -51,11 +59,11 @@ sample_event_flows[which(sample_event_flows$storm_length > 6),]
 sample_event_flows <- sample_event_flows[-which(sample_event_flows$storm_length > 6),]
 
 ### Order the sample and storm tables by start date, and add an ID
-storm_event_flows <- storm_event_flows[with(storm_event_flows, order(Field_Collection_Start_Date)), ]
+storm_event_flows <- storm_event_flows[with(storm_event_flows, order(Location_ID, Field_Collection_Start_Date)), ]
 idList <- seq(from=1, to=nrow(storm_event_flows), by = 1)
 storm_event_flows$id <- idList
 
-sample_event_flows <- sample_event_flows[with(sample_event_flows, order(Field_Collection_Start_Date)), ]
+sample_event_flows <- sample_event_flows[with(sample_event_flows, order(Location_ID, Field_Collection_Start_Date)), ]
 idList <- seq(from=1, to=nrow(sample_event_flows), by = 1)
 sample_event_flows$id <- idList
 
@@ -108,38 +116,42 @@ points(x    = storm_event_flows_small$Field_Collection_Start_Date,
 overlapStorms <- sqldf("SELECT a.id, group_concat(b.id) as matching
                        FROM storm_event_flows a
                          LEFT JOIN storm_event_flows b
-                           ON b.Field_Collection_Start_Date >= a.Field_Collection_Start_Date
-                           AND b.Field_Collection_Start_Date <= a.Field_Collection_End_Date
+                           ON a.start >= b.start
+                           AND a.end <= b.end
+                           AND a.Location_Id = b.Location_Id
                        GROUP BY a.id"
                        )
 #overlapStorms$matching <- (overlapStorms$id == overlapStorms$id2)
-
 overlapSamples <- sqldf("SELECT a.id, group_concat(b.id) as matching
                         FROM sample_event_flows a
                         LEFT JOIN sample_event_flows b
-                          ON b.Field_Collection_Start_Date >= a.Field_Collection_Start_Date
-                          AND b.Field_Collection_Start_Date <= a.Field_Collection_End_Date
+                          ON b.start >= a.start
+                          AND b.end <= a.end
+                          AND b.Location_Id = a.Location_Id
                         GROUP BY a.id"
                         )
-#overlapSamples$matching <- (overlapSamples$id == overlapSamples$id2)
 
 ### Match up flow volumes to data with SQL -------------------------
-test <- sqldf(c("CREATE INDEX s1 ON Storm(Location_ID, Field_Collection_Start_Date)",
-              "CREATE INDEX s2 ON storm_event_flows(Location_ID, Field_Collection_Start_Date, Field_Collection_End_Date)",
-              "CREATE INDEX s3 ON sample_event_flows(Location_ID, Field_Collection_Start_Date, Field_Collection_End_Date)",
+Storm <- sqldf(c("CREATE INDEX s1 ON Storm(Location_ID, start)",
+              "CREATE INDEX s2 ON storm_event_flows(Location_ID, start, end)",
+              "CREATE INDEX s3 ON sample_event_flows(Location_ID, start, end)",
               "SELECT Storm.*, 
                   storm_event_flows.new_Result_Value AS storm_event_flow_volume,
                   sample_event_flows.new_Result_Value AS sample_event_flow_volume
                 FROM Storm
                 LEFT OUTER JOIN sample_event_flows 
                   ON Storm.Location_ID = sample_event_flows.Location_ID
-                    AND Storm.Field_Collection_Start_Date >= sample_event_flows.Field_Collection_Start_Date
-                    AND Storm.Field_Collection_Start_Date <= sample_event_flows.Field_Collection_End_Date
+                    AND Storm.start >= sample_event_flows.start
+                    AND Storm.end <= sample_event_flows.end
                 LEFT OUTER JOIN storm_event_flows 
                   ON Storm.Location_ID = storm_event_flows.Location_ID
-                    AND Storm.Field_Collection_Start_Date >= storm_event_flows.Field_Collection_Start_Date
-                    AND Storm.Field_Collection_Start_Date <= storm_event_flows.Field_Collection_End_Date")
+                    AND Storm.start >= storm_event_flows.start
+                    AND Storm.end <= storm_event_flows.end")
               )
+
+### Create a list of samples with missing storm or sample volumes
+noStorm <- Storm[which(is.na(test$storm_event_flow_volume)), ]
+noSample <- test[which(is.na(test$sample_event_flow_volume)), ]
 
 ## print out a check to see if it works
 Storm[1:100, "storm_event_flow_volume"]
