c8017e35b5e311377d91c9e008c0e6157fb63f3d
Nat Kale <nathanielkale@gmail.com>
2014-04-29 17:16:31 -0700

    Criteria correctly calculated
    Pulled out the criteria calculation function to a new file.  Calcs are now
    returning values for all samples they should be (calculations for each
    parameter still need to be verified).  Awesome new graphing added.

---
+++ scripts/Calculate_Criteria.R
@@ -0,0 +1,225 @@
+
+
+criteria <- function(parameter, standard="acute", pH, hardness) {
+  ##### Return WA state numeric Do Not Exceed criteria for a given parameter -------------------------
+  #
+  # Args:
+  #   parameter (string): Which water quality parameter to check 
+  #   standard (string): in the list ("acute", "chronic", "hh")
+  #   pH (numeric): pH value associated with sample (1-14)
+  #   hardness (numeric): hardness as CaCO3 water  (ug/L)
+  # 
+  # Return:
+  #   criterion (numeric): The do not exceed value, for that parameter & standard, with the
+  #     given hardness and pH.
+  #
+  # Reference is WAC-173-201A-240.
+  # By default returns accute, but can specify a standard of "chronic" or "hh" (human health).
+  # Returns NA if there are no matching criteria; returns an error if pH or hardness is required
+  #  and is not supplied.
+  
+  if(missing(parameter)) {
+    stop("Parameter required, at a minimum.")
+  }
+  
+  missingpH   <- paste("For parameter", parameter, "pH is required.")
+  missingHard <- paste("For parameter", parameter, "hardness is required.")
+  criterion   <- ""
+  criteriaList <- c()
+  
+  # Acute standards, from WAC 173-201A-240
+  if (standard == "acute") {
+    
+    if(parameter == "Ammonia water  (ug/L)") {
+      if(missing(pH)) {stop(missingpH)}
+      criterion <- (0.275/(1+(10^(7.204-pH))))+(39/(1+(10^(pH-7.204)))*1000)
+      
+    } else if (parameter == "Arsenic water dissolved (ug/L)") {
+      criterion <- 360
+      
+    } else if (parameter == "Cadmium water  (ug/L)"){
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.944*(exp(1.128*(log(hardness))-3.828))/(1.136672 - (log(hardness)*(0.041838)))
+      
+    } else if (parameter == "Cadmium water dissolved (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- (1.136672 - ((log(hardness)) * 0.041838)) * (0.944)*(exp(1.128*(log(hardness))-3.828))
+      
+    } else if (parameter == "Chloride water  (ug/L)"){
+      criterion <- 860
+      
+    } else if (parameter == "Chlorpyrifos water  (ug/L)"){
+      criterion <- 0.083
+      
+    } else if (parameter == "Copper water  (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- (0.960) * (exp(0.9422 * (log(hardness)) - 1.464))/0.96
+      
+    } else if (parameter == "Copper water dissolved (ug/L)"){
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.960 * (exp(0.9422 * (log(hardness)) - 1.464))
+      
+    } else if (parameter == "Diazinon water  (ug/L)") {
+      criteria <- 0.3397
+      
+    } else if (parameter == "Lead water  (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.791 * (exp(1.273 * (log(hardness)) - 1.460)) / (1.46203 - (log(hardness) * (0.145712)))
+      
+    } else if (parameter == "Lead water dissolved (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.791 * (exp(1.273 * (log(hardness)) - 1.460)) 
+      
+    } else if (parameter == "Mercury water  (ug/L)") {
+      criterion <- 2.1
+      
+    } else if (parameter == "Mercury water dissolved (ug/L)") {
+      criterion <- 1.785
+      
+    } else if (parameter == "Pentachlorophenol water  (ug/L)") {
+      if(missing(pH)) {stop(missingpH)}
+      criterion <- exp((1.005 * pH) - 4.830)
+      
+    } else if (parameter == "Total PCB water  (ug/L)") {
+      criterion <- 2
+      
+    } else if (parameter == "Zinc water  (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.978 * (exp(0.8473 * (log(hardness)) + 0.8604)) / 0.978
+      
+    } else if (parameter == "Zinc water dissolved (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.978 * (exp(0.8473 * (log(hardness)) + 0.8604))
+      
+    } else {criterion <- NA}
+    
+    # Chronic standards, from WAC 173-201A-240
+  } else if (standard == "chronic"){
+    
+    if (parameter == "Arsenic water dissolved (ug/L)") {
+      criterion <- 190
+      
+    } else if (parameter == "Cadmium water  (ug/L)"){
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.909 * (exp(0.7852 * (log(hardness)) - 3.490))
+      
+    } else if (parameter == "Cadmium water dissolved (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 1.101672 - (log(hardness)*0.041838) * (0.909 * (exp(0.7852 * (log(hardness)) - 3.490)))
+      
+    } else if (parameter == "Chloride water  (ug/L)"){
+      criterion <- 230000
+      
+    } else if (parameter == "Chlorpyrifos water  (ug/L)"){
+      criterion <- 0.041
+      
+    } else if (parameter == "Copper water  (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.960 * (exp(0.9422 * (log(hardness)) - 1.464))
+      
+    } else if (parameter == "Copper water dissolved (ug/L)"){
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.96 * (0.960 * (exp(0.9422 * (log(hardness)) - 1.464)))
+      
+    } else if (parameter == "Diazinon water  (ug/L)") {
+      criterion <- 0.1699
+      
+    } else if (parameter == "Lead water  (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.791 * (exp(1.273 * (log(hardness)) - 4.705)) 
+      
+    } else if (parameter == "Lead water dissolved (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- (1.46203 - (log(hardness) * (0.145712))) * 0.791 * (exp(1.273 * (log(hardness)) - 4.705)) 
+      
+    } else if (parameter == "Mercury water  (ug/L)") {
+      criterion <- 0.012
+      
+    } else if (parameter == "Mercury water dissolved (ug/L)") {
+      criterion <- 0.0102
+      
+    } else if (parameter == "Pentachlorophenol water  (ug/L)") {
+      if(missing(pH)) {stop(missingpH)}
+      criterion <- exp(1.005 * pH) - 5.290
+      
+    } else if (parameter == "Total PCB water  (ug/L)") {
+      criterion <- 0.014
+      
+    } else if (parameter == "Zinc water  (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- exp((0.8473 * (log(hardness))) + 0.7614)
+      
+      
+    } else if (parameter == "Zinc water dissolved (ug/L)") {
+      if(missing(hardness)) {stop(missingHard)}
+      criterion <- 0.986 * (exp((0.8473 * (log(hardness))) + 0.7614))
+      
+    } else {criterion <- NA}
+    
+    # Human health standards, from ???
+  } else if (standard == "hh"){
+    
+    if (parameter == "Acenaphthylene water  (ug/L)") {
+      criterion <- 670
+      
+    } else if (parameter == "Anthracene water  (ug/L)") {
+      criterion <- 9600
+      
+    } else if (parameter == "Aresenic water dissolved (ug/L)") {
+      criterion <- 0.018
+      
+    } else if (parameter == "Benz(a)anthracene water  (ug/L)") {
+      criterion <- 0.0028
+      
+    } else if (parameter == "Benzo(a)pyrene water  (ug/L)") {
+      criterion <- 0.0028
+      
+    } else if (parameter == "Benzo(b)fluoranthene water  (ug/L)") {
+      criterion <- 0.0028
+      
+    } else if (parameter == "Benzo(b,k)fluoranthene water  (ug/L)") {
+      criterion <- 0.0028
+      
+    } else if (parameter == "Bis(2-ethylhexyl) phthalate water  (ug/L)" ) {
+      criterion <- 1.8
+      
+    } else if (parameter == "Butyl benzyl phthalate water  (ug/L)") {
+      criterion <- 1500
+      
+    } else if (parameter == "Chrysene water  (ug/L)") {
+      criterion <- 0.0028
+      
+    } else if (parameter == "Dibenzo(a,h)anthracene water  (ug/L)") {
+      criterion <- 0.0028
+      
+    } else if (parameter == "Dibutyl phthalate water  (ug/L)") {
+      criterion <- 2700
+      
+    } else if (parameter == "Diethyl phthalate water  (ug/L)") {
+      criterion <- 23000
+      
+    } else if (parameter == "Dimethyl phthalate water  (ug/L)") {
+      criterion <- 313000
+      
+    } else if (parameter == "Fluoranthene water  (ug/L)") {
+      criterion <- 300
+      
+    } else if (parameter == "Fluorene water  (ug/L)") {
+      criterion <- 1300
+      
+    } else if (parameter == "Indeno(1,2,3-cd)pyrene water  (ug/L)") {
+      criterion <- 0.0028
+      
+    } else if (parameter == "Mercury water  (ug/L)") {
+      criterion <- 0.14
+      
+    } else if (parameter == "Pyrene water  (ug/L)") {
+      criterion <- 960
+      
+    } else {criterion <- NA}
+    
+  }
+  
+  return(as.numeric(criterion))
+  
+}
--- scripts/Plot_byParam_Criteria.r
+++ scripts/Plot_byParam_Criteria.r
@@ -8,225 +8,8 @@
 require(sqldf)
 require(reshape)
 
-##### Function - Return WA state numeric Do Not Exceed criteria for a given parameter -------------------------
-#
-# parameter is a character vector
-# standard is a string in the list ("acute", "chronic", "hh")
-# pH is a numeric vector
-# hardness is a numeric vector
-#
-# Reference is WAC-173-201A-240.
-# By default returns accute, but can specify a standard of "chronic" or "hh" (human health).
-# Returns NA if there are no matching criteria; returns an error if pH or hardness is required
-#  and is not supplied.
-
-criteria <- function(parameter, standard="acute", pH, hardness) {
-
-  if(missing(parameter)) {
-    stop("Parameter required, at a minimum.")
-  }
-  
-  missingpH   <- paste("For parameter", parameter, "pH is required.")
-  missingHard <- paste("For parameter", parameter, "hardness is required.")
-  criterion   <- ""
-  criteriaList <- c()
-  
-  # Acute standards, from WAC 173-201A-240
-  if (standard == "acute") {
-  
-    if(parameter == "Ammonia water  (ug/L)") {
-      if(missing(pH)) {stop(missingpH)}
-      criterion <- (0.275/(1+(10^(7.204-pH))))+(39/(1+(10^(pH-7.204)))*1000)
-    
-    } else if (parameter == "Arsenic water dissolved (ug/L)") {
-      criterion <- 360
-    
-    } else if (parameter == "Cadmium water  (ug/L)"){
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.944*(exp(1.128*(log(hardness))-3.828))/(1.136672 - (log(hardness)*(0.041838)))
-    
-    } else if (parameter == "Cadmium water dissolved (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- (1.136672 - ((log(hardness)) * 0.041838)) * (0.944)*(exp(1.128*(log(hardness))-3.828))
-  
-    } else if (parameter == "Chloride water  (ug/L)"){
-      criterion <- 860
-      
-    } else if (parameter == "Chlorpyrifos water  (ug/L)"){
-      criterion <- 0.083
-  
-    } else if (parameter == "Copper water  (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- (0.960) * (exp(0.9422 * (log(hardness)) - 1.464))/0.96
-  
-    } else if (parameter == "Copper water dissolved (ug/L)"){
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.960 * (exp(0.9422 * (log(hardness)) - 1.464))
-      
-    } else if (parameter == "Diazinon water  (ug/L)") {
-      criteria <- 0.3397
-            
-    } else if (parameter == "Lead water  (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.791 * (exp(1.273 * (log(hardness)) - 1.460)) / (1.46203 - (log(hardness) * (0.145712)))
-  
-    } else if (parameter == "Lead water dissolved (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.791 * (exp(1.273 * (log(hardness)) - 1.460)) 
-  
-    } else if (parameter == "Mercury water  (ug/L)") {
-      criterion <- 2.1
-      
-    } else if (parameter == "Mercury water dissolved (ug/L)") {
-      criterion <- 1.785
-      
-    } else if (parameter == "Pentachlorophenol water  (ug/L)") {
-      if(missing(pH)) {stop(missingpH)}
-      criterion <- exp((1.005 * pH) - 4.830)
-  
-    } else if (parameter == "Total PCB water  (ug/L)") {
-      criterion <- 2
-  
-    } else if (parameter == "Zinc water  (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.978 * (exp(0.8473 * (log(hardness)) + 0.8604)) / 0.978
-      
-    } else if (parameter == "Zinc water dissolved (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.978 * (exp(0.8473 * (log(hardness)) + 0.8604))
-
-    } else {criterion <- NA}
-  
-    # Chronic standards, from WAC 173-201A-240
-  } else if (standard == "chronic"){
-    
-    if (parameter == "Arsenic water dissolved (ug/L)") {
-      criterion <- 190
-      
-    } else if (parameter == "Cadmium water  (ug/L)"){
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.909 * (exp(0.7852 * (log(hardness)) - 3.490))
-      
-    } else if (parameter == "Cadmium water dissolved (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 1.101672 - (log(hardness)*0.041838) * (0.909 * (exp(0.7852 * (log(hardness)) - 3.490)))
-            
-    } else if (parameter == "Chloride water  (ug/L)"){
-      criterion <- 230000
-      
-    } else if (parameter == "Chlorpyrifos water  (ug/L)"){
-      criterion <- 0.041
-      
-    } else if (parameter == "Copper water  (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.960 * (exp(0.9422 * (log(hardness)) - 1.464))
-      
-    } else if (parameter == "Copper water dissolved (ug/L)"){
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.96 * (0.960 * (exp(0.9422 * (log(hardness)) - 1.464)))
-      
-    } else if (parameter == "Diazinon water  (ug/L)") {
-      criterion <- 0.1699
-    
-    } else if (parameter == "Lead water  (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.791 * (exp(1.273 * (log(hardness)) - 4.705)) 
-      
-    } else if (parameter == "Lead water dissolved (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- (1.46203 - (log(hardness) * (0.145712))) * 0.791 * (exp(1.273 * (log(hardness)) - 4.705)) 
-      
-    } else if (parameter == "Mercury water  (ug/L)") {
-      criterion <- 0.012
-      
-    } else if (parameter == "Mercury water dissolved (ug/L)") {
-      criterion <- 0.0102
-      
-    } else if (parameter == "Pentachlorophenol water  (ug/L)") {
-      if(missing(pH)) {stop(missingpH)}
-      criterion <- exp(1.005 * pH) - 5.290
-      
-    } else if (parameter == "Total PCB water  (ug/L)") {
-      criterion <- 0.014
-      
-    } else if (parameter == "Zinc water  (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- exp((0.8473 * (log(hardness))) + 0.7614)
-
-      
-    } else if (parameter == "Zinc water dissolved (ug/L)") {
-      if(missing(hardness)) {stop(missingHard)}
-      criterion <- 0.986 * (exp((0.8473 * (log(hardness))) + 0.7614))
-      
-    } else {criterion <- NA}
-  
-    # Human health standards, from ???
-  } else if (standard == "hh"){
-    
-    if (parameter == "Acenaphthylene water  (ug/L)") {
-      criterion <- 670
-
-    } else if (parameter == "Anthracene water  (ug/L)") {
-      criterion <- 9600
-
-    } else if (parameter == "Aresenic water dissolved (ug/L)") {
-      criterion <- 0.018
-
-    } else if (parameter == "Benz(a)anthracene water  (ug/L)") {
-      criterion <- 0.0028
-
-    } else if (parameter == "Benzo(a)pyrene water  (ug/L)") {
-      criterion <- 0.0028
-
-    } else if (parameter == "Benzo(b)fluoranthene water  (ug/L)") {
-      criterion <- 0.0028
-
-    } else if (parameter == "Benzo(b,k)fluoranthene water  (ug/L)") {
-      criterion <- 0.0028
-
-    } else if (parameter == "Bis(2-ethylhexyl) phthalate water  (ug/L)" ) {
-      criterion <- 1.8
-
-    } else if (parameter == "Butyl benzyl phthalate water  (ug/L)") {
-      criterion <- 1500
-
-    } else if (parameter == "Chrysene water  (ug/L)") {
-      criterion <- 0.0028
-
-    } else if (parameter == "Dibenzo(a,h)anthracene water  (ug/L)") {
-      criterion <- 0.0028
-
-    } else if (parameter == "Dibutyl phthalate water  (ug/L)") {
-      criterion <- 2700
-
-    } else if (parameter == "Diethyl phthalate water  (ug/L)") {
-      criterion <- 23000
-
-    } else if (parameter == "Dimethyl phthalate water  (ug/L)") {
-      criterion <- 313000
-
-    } else if (parameter == "Fluoranthene water  (ug/L)") {
-      criterion <- 300
-
-    } else if (parameter == "Fluorene water  (ug/L)") {
-      criterion <- 1300
-
-    } else if (parameter == "Indeno(1,2,3-cd)pyrene water  (ug/L)") {
-      criterion <- 0.0028
-
-    } else if (parameter == "Mercury water  (ug/L)") {
-      criterion <- 0.14
-
-    } else if (parameter == "Pyrene water  (ug/L)") {
-      criterion <- 960
-    
-    } else {criterion <- NA}
-  
-  }
-  
-  return(as.numeric(criterion))
-  
-}
+# Include the function for calculating criteria
+source(paste(scriptDirectory, "Calculate_Criteria.R", sep="/"))
 
 ##### Add columns to "Storm" dataframe for pH and hardness. -------------------------
 
@@ -272,14 +55,91 @@ Storm <- sqldf(c("CREATE INDEX s1 ON Storm(Location_ID, start, end)",
 
 ##### Use the new function & rows to calculate applicable criteria -------------------------
 
-test1 <- c()
-#testLead <- Storm[which(Storm$Parameter_string == "Lead water dissolved (ug/L)"),]
+acute   <- c()
+chronic <- c()
+hh      <- c()
 
 for (i in 1:nrow(Storm)) {
-  criterion <- criteria(parameter = Storm[i,"Parameter_string"],
+  acute[i] <- criteria(parameter = Storm[i,"Parameter_string"],
                         standard  = "acute",
                         pH        = Storm[i, "pH"],
                         hardness  = Storm[i, "hardness"]
                         )
-  test1[i] <- criterion
+  chronic[i] <- criteria(parameter = Storm[i,"Parameter_string"],
+                        standard  = "chronic",
+                        pH        = Storm[i, "pH"],
+                        hardness  = Storm[i, "hardness"]
+                        )
+  hh[i] <- criteria(parameter = Storm[i,"Parameter_string"],
+                        standard  = "hh",
+                        pH        = Storm[i, "pH"],
+                        hardness  = Storm[i, "hardness"]
+                        )
 }
+
+Storm$acute   <- acute
+Storm$chronic <- chronic
+Storm$hh      <- hh
+
+Storm$acuteExceedPercent <- Storm$new_Result_Value / Storm$acute
+Storm$chronicExceedPercent <- Storm$new_Result_Value / Storm$chronic
+Storm$hhExceedPercent <- Storm$new_Result_Value / Storm$hh
+
+##### Plot out criteria by parameter (per Will Hobbs suggestion)
+pdf(paste(outputDirectory, "concentration_criteria_plots.pdf", sep="/"), width=11, height=8.5)
+
+mar.default = c(5, 4, 4, 2) + 0.1
+
+# Acute values vs. criteria
+par(mar = mar.default + c(0, 12, 0, 0))
+storm.acute <- Storm[-which(is.na(Storm$acute)), ]
+storm.acute$Parameter_string <- factor(storm.acute$Parameter_string)
+
+ylimits <- c(1, length(levels(storm.acute$Parameter_string)))
+xlimits <- c(min(storm.acute$new_Result_Value), max(storm.acute$new_Result_Value))
+
+plot(storm.acute$new_Result_Value, 
+     storm.acute$Parameter_string,
+     pch  = -124,
+     cex  = 0.5,
+     col  = "darkgray",
+     log  = "x",
+     xlab = "Sampled Concentration (ug/L)",
+     ylab = "",
+     main = "Range of Concentrations with acute criteria",
+     yaxt = "n"
+     )
+
+axis(side = 2, 
+     at   = 1:16, 
+     las  = 1,
+     labels = levels(storm.acute$Parameter_string))
+
+# Add additional labelling/decoration to each data series
+j <- 1
+for (param in levels(storm.acute$Parameter_string)) {
+  
+  xMin <- min(storm.acute$new_Result_Value)
+  xMax <- max(storm.acute$new_Result_Value)
+  abline(h   = j,
+         col = "lightgray"
+         )
+  
+  # Add min and max labels at the ends of each data series
+  theMin <- min(storm.acute$new_Result_Value[which(storm.acute$Parameter_string == param)])
+  theMax <- max(storm.acute$new_Result_Value[which(storm.acute$Parameter_string == param)])
+   
+  text(theMin, j, labels = c(as.character(theMin)), pos = 2, offset = 0.5, cex = 0.85 )
+  text(theMax, j, labels = c(as.character(theMax)), pos = 4, offset = 0.5, cex = 0.85 )
+  
+  # Add the min and max of the criteria to each data series
+  acuteMin <- min(storm.acute$acute[which(storm.acute$Parameter_string == param)])
+  acuteMax <- max(storm.acute$acute[which(storm.acute$Parameter_string == param)])
+  
+  text(acuteMin, j, labels = c("|"), col = "blue")
+  text(acuteMax, j, labels = c("|"), col = "red" )
+  
+  j <- j + 1
+  
+}
+dev.off()
